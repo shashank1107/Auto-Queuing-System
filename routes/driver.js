73427af2ade1ce475d645f708d40624d983947a9
shashank.pareta <shashank.pareta@innoplexus.com>
2017-04-22 23:01:59 +0530

    all queries done

--- routes/driver.js
+++ routes/driver.js
@@ -6,31 +6,34 @@ var expressValidator = require('express-validator');
 var router = express.Router();
 router.use(expressValidator());
 
-router.post('/select/:requestid', function(req, res){
+router.post('/select/:requestid',
 
-      req.checkParams("requestid", "Invalid value").notEmpty().isString();
-      req.checkBody("driverid", "Invalid value").notEmpty().isString();
-      var errors = req.validationErrors(true);
-      if (errors) {
-          response = store.getResponse(400);
-          response.error = error.response.body;
-          return res.status(400).send(response);
-      }
-      next()
-  },
+  //   function(req, res){
+  //
+  //     req.checkParams("requestid", "Invalid value").notEmpty().isString();
+  //     req.checkBody("driverid", "Invalid value").notEmpty().isString();
+  //     var errors = req.validationErrors(true);
+  //     if (errors) {
+  //         response = store.getResponse(400);
+  //         response.error = error.response.body;
+  //         return res.status(400).send(response);
+  //     }
+  //     next()
+  // },
     controller.selectride
 );
 
-router.post('/list',  function(req, res){
-      req.checkBody("driverid", "Invalid value").notEmpty().isString();
-      var errors = req.validationErrors(true);
-      if (errors) {
-          response = store.getResponse(400);
-          response.error = error.response.body;
-          return res.status(400).send(response);
-      }
-      next()
-  },
+router.post('/list',
+  //   function(req, res){
+  //     req.checkBody("driverid", "Invalid value").notEmpty().isString();
+  //     var errors = req.validationErrors(true);
+  //     if (errors) {
+  //         response = store.getResponse(400);
+  //         response.error = error.response.body;
+  //         return res.status(400).send(response);
+  //     }
+  //     next()
+  // },
     controller.getList
 );
 
